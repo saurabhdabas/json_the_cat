907c891b13800edaaa21ed3b30822098a7fe2b60
saurabhdabas <developerdabas@gmail.com>
2022-02-17 21:39:41 +0000

    Refactored the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,18 @@
 const request = require('request');
 
-const args = process.argv;
-const breed = args.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function (error, response, body) {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, response, body) {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
   const data = JSON.parse(body);
-  console.log(data);
-  if(!data[0]){
-    console.log("No such breed");
+  if(data[0]){
+    console.log(data[0].description);
   }
   if(error){
-    console.log(`error: ${error}`);
-  }
-  else{
-    console.log(data[0].description); 
+    callback();
   }
 });
+};
+module.exports = { fetchBreedDescription };
